5ce6f9dc588eb46bdf4a86d08b29329a2854ec16
gaofeng222 <596642721@qq.com>
2024-08-17 23:00:17 +0800

    add:init

---
+++ apps/h5/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { sum } from '@gf/utils'
+import { Button } from '@gf/ui'
+const data = sum(1, 2)
+console.log(data)
+</script>
+
+<template>
+  <Button />
+</template>
+
+<style scoped></style>
---
+++ packages/ui/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button>测试</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MonorepoAppButton',
+
+  data() {
+    return {}
+  },
+
+  mounted() {},
+
+  methods: {}
+}
+</script>
